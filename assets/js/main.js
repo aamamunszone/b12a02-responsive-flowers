c39ffa945ea24f669ad73a944abcc35e190d0c36
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-07-25 19:19:43 +0600

    feat: added button alert, hover effects, basic animations; commented extra UI tweaks for future use

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// function handleAddToCart() {
+//   // Show Modal
+//   const modal = document.getElementById("btn-modal");
+//   modal.classList.remove("hidden");
+//   modal.classList.add("flex");
+// }
+
+// function closeModal() {
+//   // Close Modal
+//   const modal = document.getElementById("btn-modal");
+//   modal.classList.remove("flex");
+//   modal.classList.add("hidden");
+// }
